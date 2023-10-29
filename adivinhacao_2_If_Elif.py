200d2d30abecbec5b63e79356f09ad4755fa0a69
MarcosSouzaa <marcossouza.git@gmail.com>
2023-10-29 16:39:19 -0300

    usando if e elif rename

---
+++ adivinhacao_2_If_Elif.py
@@ -0,0 +1,25 @@
+# Jogo de Adivinhação
+print("********************************************")
+print('Jogo de advinhação!')
+print("********************************************")
+
+numero_secreto = 5
+
+chute = int(input('Digite um número de 0 a 10: '))
+
+print('\nNossa!! Você digitou o número ', chute)
+
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if acertou:
+    print('\nParabéns! Você acertou! Seu dia será ótimo!')
+else:
+    if maior:
+        print('\nVocê errou feio! Seu número é maior que o Número secreto!!!')
+
+    elif menor:
+        print('\nVocê errou feio! Seu número é menor que o Número secreto!!!')
+
+print('\nFim de jogo!')
