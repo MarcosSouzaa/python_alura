59b27fe5380245f47b69dc8b790faebe6e54c374
MarcosSouzaa <marcossouza.git@gmail.com>
2023-10-29 19:22:54 -0300

    renomeando

---
+++ exer3_contador_while.py
@@ -0,0 +1,18 @@
+# Despertador
+
+contador = 1
+
+while contador <= 10:
+    print(contador)
+    contador = contador+2
+    if contador == 5:
+        contador = contador + 2
+
+'''
+ele não vai imprimir o 5
+1
+3
+7
+9
+
+'''
