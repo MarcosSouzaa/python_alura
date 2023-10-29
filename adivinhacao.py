b54eb3bc78adbac3743e2494d6a0c9dc9537aa0c
MarcosSouzaa <marcossouza.git@gmail.com>
2023-10-28 21:37:17 -0300

    Commit do jogo de adivinhação

---
+++ adivinhacao.py
@@ -0,0 +1,17 @@
+# Jogo de Adivinhação
+print("********************************************")
+print('Jogo de advinhação!')
+print("********************************************")
+
+numero_secreto = 0
+
+chute = int(input('Digite um número de 0 a 10: '))
+
+print('\nNossa!! Você digitou o número ', chute)
+
+if numero_secreto == chute:
+     print('\nParabéns! Você acertou!')
+else:
+    print('\nVocê errou feio! KKKKK')
+
+
