97f0e119067fb84449ab1c685469a2ae63cdc6e4
MarcosSouzaa <marcossouza.git@gmail.com>
2023-10-29 20:13:51 -0300

    exercício usando format String

---
+++ exerc4_criando_string_format.py
@@ -0,0 +1,9 @@
+# Criando uma string format
+#"Em 2017 o Carnaval acontece em fevereiro do dia 24 até o dia 28"#
+
+dia_ini = 24
+dia_fim = 28
+mes = "fevereiro"
+ano = 2017
+
+print('Em {} o carnaval acontece em {} do dia {} até o dia {}'.format(ano, mes, dia_ini, dia_fim))
