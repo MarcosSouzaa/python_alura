2b5f929946abd3f68bf0d7cc4b75f3db5c546153
MarcosSouzaa <marcossouza.git@gmail.com>
2023-10-30 20:54:15 -0300

    Do while para o For

---
+++ exerc5_doWhile_para_for.py
@@ -0,0 +1,21 @@
+# Modificando do while para o for
+
+# Utilizando while
+contador = 1
+while(contador <= 10):
+    print(contador)
+    contador = contador + 1
+
+# Utilizando For
+for contador in range(1, 11):
+    print(contador)
+
+# Utilizando While iterando de 3 em 3
+contador = 1
+while(contador <= 10):
+    print(contador)
+    contador = contador + 3
+
+# Utilizando For iterando de 3 em 3
+for contador in range(1, 11, 3):
+    print(contador)
