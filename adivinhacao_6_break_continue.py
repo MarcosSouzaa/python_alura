4999e3edf1cc69f0ca74f220fb0efb5f1e5385fa
MarcosSouzaa <marcossouza.git@gmail.com>
2023-10-31 19:48:51 -0300

    implementando nível de dificuldades com if e elif

--- adivinhacao_6_break_continue.py
+++ adivinhacao_6_break_continue.py
@@ -6,16 +6,27 @@ print("********************************************")
 print('Jogo de advinhação!')
 print("********************************************")
 
-numero_secreto = randint(0, 10)
+numero_secreto = randint(0, 100)
+total_de_tentativas = 0
 
-total_de_tentativas = 3
+print('\nQual o nível de dificuldade?')
+print('\n(1) Fácil, (2) Médio, (3 )Difícil\n')
+
+nivel = int(input('Defina o nível: '))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
-    chute = int(input('\nDigite um número de 0 a 10: '))
+    chute = int(input('\nDigite um número de 0 a 100: '))
     print('\nVocê digitou o número ', chute)
 
-    if chute < 0 or chute > 10:
+    if chute < 0 or chute > 100:
         print('\n"VOCÊ DEVE DIGITAR UM NÚMERO DE 0 a 10!"')
         continue
 
